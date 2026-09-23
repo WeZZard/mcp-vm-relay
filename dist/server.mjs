d2a86ddd44d79c2234bde00220f8b5b6edd3ab2b
WeZZard <me@wezzard.com>
2026-09-24 04:16:55 +0800

    Release v0.5.0

--- dist/server.mjs
+++ dist/server.mjs
@@ -29449,7 +29449,7 @@ async function relayCall(host, raw, options = {}) {
 
 // src/server.ts
 var SERVER_NAME = "relay";
-var SERVER_VERSION = true ? "0.4.0" : JSON.parse(readFileSync(fileURLToPath2(new URL("../package.json", import.meta.url)), "utf8")).version;
+var SERVER_VERSION = true ? "0.5.0" : JSON.parse(readFileSync(fileURLToPath2(new URL("../package.json", import.meta.url)), "utf8")).version;
 var STATUS_TOOL = "relay_status";
 var TRAJECTORY_TOOL = "relay_trajectory";
 var PLUGIN_TOOL_PREFIX = "mcp__plugin_mcp-vm-relay_relay__";
